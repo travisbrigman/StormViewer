de6c1ac1f8be86209505348789ee0510e382cd19
travisbrigman <travis1000@gmail.com>
2021-06-22 11:04:33 -0500

    adds async loading of data

--- StormViewer/ViewController.swift
+++ StormViewer/ViewController.swift
@@ -17,14 +17,16 @@ class ViewController: UITableViewController {
         // Do any additional setup after loading the view.
         let fm = FileManager.default
         let path = Bundle.main.resourcePath!
-        let items = try! fm.contentsOfDirectory(atPath: path)
-
-        for item in items {
-            if item.hasPrefix("nssl") {
-                pictures.append(item)
+        DispatchQueue.global().async {
+            let items = try! fm.contentsOfDirectory(atPath: path)
+            
+            for item in items {
+                if item.hasPrefix("nssl") {
+                    self.pictures.append(item)
+                }
             }
+            self.pictures.sort()
         }
-        pictures.sort()
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
